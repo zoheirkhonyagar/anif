c640ce300c8ff826192449a0007b29cf01cbf8ae
zoheir <zoheirkhonyagar@gmail.com>
2017-10-15 13:47:08 +0330

    break index method to 2 other mehtods

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -10,12 +10,24 @@ class StoreController extends Controller
 {
     public function index()
     {
+        $sortedWithOff = $this->getOfferStores();
+
+        $storesWithRank = $this->getBestStores();
+
+        return view('main.main-page.index' , compact( 'sortedWithOff' , 'storesWithRank'));
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOfferStores()
+    {
         $stores = Store::all();
         $temp_store = [];
-        foreach ($stores as $store)
-        {
+        foreach ($stores as $store) {
             $tmp = DB::table('products')->select(DB::raw('max(off) as maxOff'))->where('store_id', $store['id'])->first();
-            if ($tmp->maxOff){
+            if ($tmp->maxOff) {
                 $store['max_off'] = $tmp->maxOff;
                 $temp_store [] = $store;
             }
@@ -24,13 +36,16 @@ class StoreController extends Controller
 
         $temp_store = collect($temp_store);
 
-        $storesWithRank = Store::orderBy('rank', 'desc')->limit(5)->get(); //اطلاعات 5 تا از بهترین مجموعه های آنیف
+        $sortedWithOff = $temp_store->sortByDesc('max_off')->values()->all();
+        return $sortedWithOff; // تمام مجموعه های آنیف رو بر اساس بالاترین تخفیف سورت می کنه
+    }
 
-        $sortedWithOff = $temp_store->sortByDesc('max_off')->values()->all(); // تمام مجموعه های آنیف رو بر اساس بالاترین تخفیف سورت می کنه
-
-        //view('main.main-page.components.best-restaurant' , compact( 'storesWithRank') );
-
-        return view('main.main-page.index' , compact( 'sortedWithOff' , 'storesWithRank'));
-
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function getBestStores()
+    {
+        $storesWithRank = Store::orderBy('rank', 'desc')->limit(5)->get();
+        return $storesWithRank; //اطلاعات 5 تا از بهترین مجموعه های آنیف
     }
 }
