d423342818edaf07f1f2cd730ed0c07f96e4249e
edr9 <h.ehsan24@gmail.com>
2018-01-31 09:52:53 +0330

    make city model and controller and api fow show all cities

---
+++ app/Http/Controllers/Api/v1/CityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\City;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CityController extends apiController
+{
+
+    public function getAllCities()
+    {
+
+        $cities = City::all();
+
+
+        return $this->respondTrue($cities);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::group(['prefix'=> 'v1', 'namespace'=> 'Api\v1'], function () {
     $this->post('login', 'UserController@login');
     $this->post('register', 'UserController@register');
     $this->post('sendVerifyCode', 'UserController@sendVerifyCode');
+    $this->get('getCities', 'CityController@getAllCities');
 
 
     Route::middleware('auth:api')->group(function ()
